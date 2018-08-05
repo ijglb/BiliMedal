1e1bcfcf3950e002c68df47d8d5fab374eaa84d6
ijglb <jglb@vip.qq.com>
2018-08-05 13:33:42 +0800

    CLOSE_WAIT问题导致的CPU100%和停止抓取勋章数据？

--- BiliMedalCollection/Utils/BiliBili.cs
+++ BiliMedalCollection/Utils/BiliBili.cs
@@ -8,20 +8,22 @@ namespace BiliMedalCollection.Utils
         {
             try
             {
-                HttpHelper http = new HttpHelper()
-                .AddHeaders("Referer", string.Format("https://live.bilibili.com/{0}", roomID))
-                .AddHeaders("Origin", "https://live.bilibili.com");
-                //取uid
-                var json = http.GetJObjectAsync(string.Format("https://api.live.bilibili.com/room/v1/Room/get_info?room_id={0}", roomID)).Result;
-                if (json["code"].ToString() != "0")//错误，未找到房间
-                    return null;
-                string uid = json["data"]["uid"].ToString();
-                //通过uid取粉丝榜
-                json = http.GetJObjectAsync(string.Format("https://api.live.bilibili.com/rankdb/v1/RoomRank/webMedalRank?ruid={0}", uid)).Result;
-                if (json["data"]["list"].HasValues)
-                    return json["data"]["list"][0]["medal_name"].ToString();
-                else
-                    return null;
+                using (HttpHelper http = new HttpHelper())
+                {
+                    http.AddHeaders("Referer", string.Format("https://live.bilibili.com/{0}", roomID))
+                        .AddHeaders("Origin", "https://live.bilibili.com");
+                    //取uid
+                    var json = http.GetJObjectAsync(string.Format("https://api.live.bilibili.com/room/v1/Room/get_info?room_id={0}", roomID)).Result;
+                    if (json["code"].ToString() != "0")//错误，未找到房间
+                        return null;
+                    string uid = json["data"]["uid"].ToString();
+                    //通过uid取粉丝榜
+                    json = http.GetJObjectAsync(string.Format("https://api.live.bilibili.com/rankdb/v1/RoomRank/webMedalRank?ruid={0}", uid)).Result;
+                    if (json["data"]["list"].HasValues)
+                        return json["data"]["list"][0]["medal_name"].ToString();
+                    else
+                        return null;
+                }
             }
             catch (Exception)
             {
--- BiliMedalCollection/Utils/Http/HttpHelper.cs
+++ BiliMedalCollection/Utils/Http/HttpHelper.cs
@@ -8,31 +8,14 @@ using System.Threading.Tasks;
 
 namespace BiliMedalCollection.Utils
 {
-    public class HttpHelper
+    public class HttpHelper: HttpClient
     {
-        #region HttpClient
-        private static HttpClient _Client;
-        private static DateTime _LastCreatTime;
-        static HttpHelper()
-        {
-            CreatNewHttpClient();
-        }
-        static void CreatNewHttpClient()
-        {
-            _Client = new HttpClient();
-            _Client.Timeout = TimeSpan.FromSeconds(20);
-            _LastCreatTime = DateTime.Now;
-        }
-        #endregion
-
         private HttpContent _httpContent;
 
         public HttpHelper()
         {
-            if (DateTime.Now - _LastCreatTime > TimeSpan.FromDays(1))
-                CreatNewHttpClient();
-            _Client.DefaultRequestHeaders.Clear();
-            _Client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36");
+            base.DefaultRequestHeaders.Clear();
+            base.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36");
             _httpContent = new StringContent("");
         }
         /// <summary>
@@ -43,7 +26,7 @@ namespace BiliMedalCollection.Utils
         /// <returns></returns>
         public HttpHelper AddHeaders(string name, string value)
         {
-            _Client.DefaultRequestHeaders.Add(name, value);
+            base.DefaultRequestHeaders.Add(name, value);
             return this;
         }
         /// <summary>
@@ -69,17 +52,13 @@ namespace BiliMedalCollection.Utils
         /// <returns></returns>
         public HttpHelper AddStringContent(string str) => AddHttpContent(new StringContent(str, Encoding.UTF8, "application/x-www-form-urlencoded"));
 
-        public async Task<HttpResponseMessage> GetAsync(string url) => await _Client.GetAsync(url);
-
-        public async Task<string> GetStringAsync(string url) => await _Client.GetStringAsync(url);
-
         public async Task<JObject> GetJObjectAsync(string url)
         {
-            string jsonStr = await _Client.GetStringAsync(url);
+            string jsonStr = await base.GetStringAsync(url);
             return JsonConvert.DeserializeObject<JObject>(jsonStr);
         }
 
-        public async Task<HttpResponseMessage> PostAsync(string url) => await _Client.PostAsync(url, _httpContent);
+        public async Task<HttpResponseMessage> PostAsync(string url) => await base.PostAsync(url, _httpContent);
 
         public async Task<string> PostStringAsync(string url)
         {
